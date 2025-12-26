a0ebcd2ab3f5fab676f62da50f02eb163b3b2a9e
ultimatuum <stormberg2003@gmail.com>
2025-12-26 19:24:07 +0800

    add: proxy

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,37 @@
-fn main() {
-    println!("Hello, world!");
+use tokio::io::copy_bidirectional;
+use tokio::net::{TcpListener, TcpStream};
+
+use futures::FutureExt;
+use std::env;
+use std::error::Error;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    let listen_addr = env::args()
+        .nth(1)
+        .unwrap_or_else(|| "127.0.0.1:8132".to_string());
+    let server_addr = env::args()
+        .nth(2)
+        .expect("remote server (e.g, 78.43.2.5:88923)");
+
+    println!("Listening on: {listen_addr}");
+    println!("Proxying to: {server_addr}");
+
+    let listener = TcpListener::bind(listen_addr).await?;
+
+    while let Ok((mut inbound, _)) = listener.accept().await {
+        let mut outbound = TcpStream::connect(server_addr.clone()).await?;
+
+        tokio::spawn(async move {
+            copy_bidirectional(&mut inbound, &mut outbound)
+                .map(|r| {
+                    if let Err(e) = r {
+                        println!("Failed to transfer; error={e}");
+                    }
+                })
+                .await
+        });
+    }
+
+    Ok(())
 }
